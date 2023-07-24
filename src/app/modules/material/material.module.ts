1cdb5a9f163d12943a6b5e7d7ad8c574012b2f8e
Siyabonga <hlongwanesiyabonga6@gmail.com>
2023-07-24 23:18:22 +0200

    Installed Flex Layout & Angular Material Module, Added Mat Module File

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+
+const MatComponents = [
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatSelectModule
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatComponents
+  ],
+  exports: [
+    CommonModule,
+    MatComponents
+  ]
+})
+export class MaterialModule { }
