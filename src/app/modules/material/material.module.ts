0df5d140666d76762ff4272458fa23f7717e9d6a
Siyabonga <hlongwanesiyabonga6@gmail.com>
2023-07-26 10:13:43 +0200

    Toolbar Design

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -14,6 +14,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatBadgeModule} from '@angular/material/badge';
 
 const MatComponents = [
   MatButtonModule,
@@ -29,7 +30,8 @@ const MatComponents = [
   MatProgressSpinnerModule,
   MatSnackBarModule,
   MatToolbarModule,
-  MatSelectModule
+  MatSelectModule,
+  MatBadgeModule
 ]
 
 
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    SidenavComponent
+    SidenavComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
     FlexLayoutModule
   ], 
-  exports: [FlexLayoutModule, SidenavComponent]
+  exports: [FlexLayoutModule, SidenavComponent, ToolbarComponent]
 })
 export class SharedModule { }
